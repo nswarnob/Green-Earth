4f07ed3ba195a3cd4daf293bf0477cd8a8bf0165
Sharif Uddin Arnob <nswarnob@gmail.com>
2025-09-09 23:00:52 +0600

    All feature added perfectly

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// all container
 const categoriesContainer = document.getElementById("categories-container");
 const cardContainer = document.getElementById("card-container");
-const allPlantsBtn = document.getElementById("all-plants")
+const cartContainer = document.getElementById("cart-container")
+
+
 
 // category btn form the api and display in dom 
 const loadCategory = () => {
@@ -8,56 +11,60 @@ const loadCategory = () => {
         .then(res => res.json())
         .then(data => {
             const categories = data.categories;
-           displayCategory(categories)
+            displayCategory(categories)
         })
+
 };
 
-const displayCategory = (categories)=>{
-     categories.forEach(cat => {
-                categoriesContainer.innerHTML += `<button id="${cat.id}" class="category-btn cursor-pointer block mt-3 font-semibold text-[15px] ">${cat.category_name}</button>`
-            });
+const displayCategory = (categories) => {
+    categories.forEach(cat => {
+        categoriesContainer.innerHTML += `<button id="${cat.id}" class="category-btn cursor-pointer block mt-3 font-semibold text-[15px] ">${cat.category_name}</button>`
+    });
 
 
-// active button feature 
-categoriesContainer.addEventListener("click", (e)=>{
+    // active button feature 
+    categoriesContainer.addEventListener("click", (e) => {
+        const allBtn = document.querySelectorAll(".category-btn")
+        allBtn.forEach(btn => {
+            btn.classList.remove("bg-[#15803D]", "text-white", "px-3", "py-1", "rounded-xl")
+        })
 
-    const allBtn = document.querySelectorAll(".category-btn")
-allBtn.forEach(btn =>{
-    btn.classList.remove("bg-[#15803D]", "text-white", "px-3", "py-1", "rounded-xl")
-})
 
-   if(e.target.localName === 'button'){
-       e.target.classList.add("bg-[#15803D]", "text-white", "px-3", "py-1", "rounded-xl")
-    loadPlanByCategory(e.target.id);  
-    }
-    
-   
-})
+        if (e.target.localName === 'button') {
+            e.target.classList.add("bg-[#15803D]", "text-white", "px-3", "py-1", "rounded-xl")
+
+            if (e.target.id === "all-plants") {
+                displayPlanByAll(e.target.id)
+            }
+            else {
+                loadPlanByCategory(e.target.id);
+            }
+        }
+    })
 };
-
 
 // showing plants by clicking category 
-const loadPlanByCategory = (categoryId)=>{
+const loadPlanByCategory = (categoryId) => {
     fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
-    .then(res => res.json())
-    .then(data=>{
-        displayPlanByCategory(data.plants);
-    })
+        .then(res => res.json())
+        .then(data => {
+            displayPlanByCategory(data.plants);
+        })
 }
 
-const displayPlanByCategory =(plants)=>{
-
-    plants.forEach(plant =>{
+const displayPlanByCategory = (categorisedPlants) => {
+    cardContainer.innerHTML = ' ';
+    categorisedPlants.forEach(plant => {
         cardContainer.innerHTML += `
-         <div class="max-w-xs bg-white rounded-2xl shadow-md overflow-hidden border border-gray-100">
+         <div class="details max-w-xs bg-white rounded-2xl shadow-md overflow-hidden border border-gray-100">
                     <!-- Content -->
                     <div class="p-4 space-y-3">
                         <!-- Image Placeholder -->
                         <div class="bg-gray-200 rounded-md">
-                        <img src="${plant.image}" class="h-[200px] w-[400px] rounded-sm">
+                        <img src="${plant.image}" class="h-[200px] w-[300px] rounded-sm object-cover">
                         </div>
                         <!-- Title -->
-                        <h2 class="text-sm font-semibold text-gray-800">${plant.name}</h2>
+                        <h2 class="text-sm font-semibold text-gray-800 plant-title">${plant.name}</h2>
 
                         <!-- Description -->
                         <p class="text-gray-500 text-xs">
@@ -69,12 +76,12 @@ const displayPlanByCategory =(plants)=>{
                             <span class="bg-green-100 text-green-700 text-xs font-medium px-3 py-1 rounded-full">
                                ${plant.category}
                             </span>
-                            <span class="text-gray-800 font-semibold">${plant.price}</span>
+                            <span class="text-gray-800 font-semibold plant-price">${plant.price}</span>
                         </div>
 
                         <!-- Button -->
-                        <button
-                            class="w-full bg-green-600 hover:bg-green-700 text-white text-sm font-medium py-2 rounded-full mt-3">
+                        <button 
+                            class="cart-btn w-full bg-green-600 hover:bg-green-700 text-white text-sm font-medium py-2 rounded-full mt-3">
                             Add to Cart
                         </button>
                     </div>
@@ -82,13 +89,67 @@ const displayPlanByCategory =(plants)=>{
         `
     })
 
+
+}
+
+// All buttons feature and by default 
+const displayPlanByAll = () => {
+    fetch(`https://openapi.programming-hero.com/api/plants`)
+        .then(res => res.json())
+        .then(data => {
+            const allPlants = data.plants
+            displayPlanByCategory(allPlants)
+        })
+
+
+
+}
+
+// add to the cart
+cardContainer.addEventListener("click", (plant) => {
+    if (plant.target.classList.contains("cart-btn")) {
+
+        const card = plant.target.closest(".details");
+        const plantTitle = card.querySelector(".plant-title").innerText;
+        const plantPrice = card.querySelector(".plant-price").innerText;
+
+        cartContainer.innerHTML += `
+         <div  class="cart-box flex items-center justify-between p-3 rounded-lg bg-[#F0FDF4]">
+                    <div>
+                        <h2 class="font-semibold">${plantTitle}</h2>
+                        <p class="text-gray-400" data-price="${plantPrice}">${plantPrice}x 1</p>
+                    </div>
+                    <div id="cross" class="cut-btn text-gray-400 font-medium bg-white h-6 w-6 text-center rounded-full hover:bg-gray-400 hover:text-white hover:transition-transform">X</div>
+                </div>
+                
+        `
+    }
+    updateTotal();
+});
+
+// remove cart & substract
+cartContainer.addEventListener("click", (plant) => {
+    if (plant.target.classList.contains("cut-btn")) {
+        plant.target.closest(".cart-box").remove(); 
+        updateTotal(); 
+    }
+});
+
+// total 
+const updateTotal =()=> {
+    const totalPriceEl = document.getElementById("total");
+    const cartBoxes = cartContainer.querySelectorAll(".cart-box");
+    let total = 0;
+
+    cartBoxes.forEach(box => {
+        const price = box.querySelector("p");
+        total += Number(price.dataset.price);
+    });
+
+    totalPriceEl.innerText = total;
 }
 
 
-
-
-
-
-
-
+// function calling here 
 loadCategory();
+displayPlanByAll();
